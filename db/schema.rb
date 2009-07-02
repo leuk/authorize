3d9cab7e4e8f93128c3bb8696a21935b5160d466
leuk <pathsene@netsyscom.biz>
2009-07-02 08:38:58 +0200

    user avatars added

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090701022956) do
+ActiveRecord::Schema.define(:version => 20090702061713) do
 
   create_table "roles", :force => true do |t|
     t.string   "title"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(:version => 20090701022956) do
     t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
